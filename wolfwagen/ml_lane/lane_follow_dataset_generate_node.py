0a3470e47d00c99979297816dcc06d8e824dddf8
arnaavgoyal <58274830+arnaavgoyal@users.noreply.github.com>
2023-04-25 00:10:32 -0400

    dataset generation script for lane following

---
+++ wolfwagen/ml_lane/lane_follow_dataset_generate_node.py
@@ -0,0 +1,64 @@
+import rclpy
+from rclpy.node import Node
+from sensor_msgs.msg import Image
+from std_msgs.msg import Int64
+import cv2 as cv
+import numpy as np
+# from matplotlib.pyplot import hsv
+from cv_bridge import CvBridge
+import threading
+import time
+import tensorflow as tf
+keras = tf.keras
+
+DATA_SAVE_PATH = "lanefollow/data/img_"
+DATA_FILE_EXTENSION = ".png"
+
+steering = None
+file_idx = 0
+img = None
+bridge = CvBridge()
+
+def zed_callback(msg: Image = None) -> None :
+    global img
+    img = msg
+
+def steering_callback(msg: Int64 = None) -> None:
+    global steering
+    steering = msg
+
+def main(args=None) -> None :
+    global img
+
+    rclpy.init(args=args)
+    node = Node("lane_image_dataset_generate_node")
+
+    node.create_subscription(
+        Image,
+        '/zed2i/zed_node/rgb_raw/image_raw_color',
+        zed_callback,
+        1
+    )
+
+    node.create_subscription(
+        Int64,
+        'pid_steering',
+        steering_callback,
+        1
+    )
+
+    thread = threading.Thread(target=rclpy.spin, args=(node, ), daemon=True)
+    thread.start()
+
+    rate = node.create_rate(1, node.get_clock())
+
+    while rclpy.ok() :
+        cv.imwrite(DATA_SAVE_PATH + str(file_idx) + "_s" + str(steering.data) + DATA_FILE_EXTENSION, bridge.imgmsg_to_cv2(img))
+        file_idx += 1
+        rate.sleep()
+    
+    rclpy.shutdown()
+
+
+if __name__ == "__main__" :
+    main()
