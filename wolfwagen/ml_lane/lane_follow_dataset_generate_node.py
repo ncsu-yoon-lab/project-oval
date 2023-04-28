e82d0923890c7bd66c996bfa62145f7a79a43c61
arnaavgoyal <58274830+arnaavgoyal@users.noreply.github.com>
2023-04-28 15:42:50 -0400

    fix for lane follow dataset gen

--- wolfwagen/ml_lane/lane_follow_dataset_generate_node.py
+++ wolfwagen/ml_lane/lane_follow_dataset_generate_node.py
@@ -1,3 +1,5 @@
+#!/usr/bin/env python
+
 import rclpy
 from rclpy.node import Node
 from sensor_msgs.msg import Image
@@ -15,24 +17,25 @@ DATA_SAVE_PATH = "lanefollow/data/"
 DATA_NAME_PRE = "img_"
 DATA_FILE_EXTENSION = ".png"
 
-steering = None
+steering = 0
 file_idx = 0
 img = None
 bridge = CvBridge()
-
+new_supplied = False
 def save_dataset(dataset: pd.DataFrame):
     dataset.to_csv(DATA_SAVE_PATH + "meta.csv")
 
 def zed_callback(msg: Image = None) -> None :
-    global img
+    global img, new_supplied
     img = msg
+    new_supplied = True
 
 def steering_callback(msg: Int64 = None) -> None:
     global steering
     steering = msg
 
 def main(args=None) -> None :
-    global img, steering
+    global img, steering, file_idx
 
     rclpy.init(args=args)
     node = Node("lane_image_dataset_generate_node")
@@ -60,10 +63,14 @@ def main(args=None) -> None :
         rate = node.create_rate(1, node.get_clock())
 
         while rclpy.ok() :
+            if not new_supplied:
+                continue
             fname = DATA_NAME_PRE + str(file_idx) + DATA_FILE_EXTENSION
             cv.imwrite(DATA_SAVE_PATH + fname, bridge.imgmsg_to_cv2(img))
-            row = pd.Series({"img": fname, "steering": steering})
+            row = pd.Series({"img": fname, "steering": steering.data})
             dataset = pd.concat([dataset, row.to_frame().T], ignore_index=True)
+            print(steering.data)
+            new_supplied = False
             file_idx += 1
             rate.sleep()
     
