3e9dd5fbd0f437dbbd1ea82ad394c615c6ca5108
anglia <anglia@wolfwagen.com>
2023-06-30 14:54:17 -0400

    update LaneDetectionVastar.py

--- wolfwagen/AStar/test/testAstar.py
+++ wolfwagen/AStar/test/testAstar.py
@@ -1,26 +1,29 @@
 import sys
 sys.path.insert(0, '/home/anglia/ros2_ws2/src/wolfwagen/wolfwagen/AStar/simulation')
 import runsim
+from pathlib import Path
+
 
 num_trials = 100
 iterations = 20
 successful_trials = 0
-startX = 0
-startY = 2
-targetX = 1
-targetY = 0
+startX = 1
+startY = 0
+targetX = 0
+targetY = 2
 
+print(Path.cwd())
 map_file = '../files/map01.txt'
 costs = '../files/costs.txt'
 straight_line = '../files/straight_line.txt'
 for trial in range(num_trials):
     sim = runsim.RunSim(map_file, iterations, costs, straight_line, startX, startY, targetX, targetY)
     solution = sim.run()
-
+    action = 0
     print(solution)
-
-    while not sim.goal_condition_met():
+    while action is not None:
         action = sim.get_next_action()
+        # print(action)
         sim.env.actuate_env(action)
     print("----------- trial " + str(trial) + " done ---------------")
     if sim.goal_condition_met():
