b33ddbde3a441fb6b4fc4e53c1419a0dfcd0918e
ColeMalinchock1 <malinchc@gmail.com>
2025-02-01 19:51:18 -0500

    Driver and xbox controller tested and ready for steering

--- project-oval/driver_node.py
+++ project-oval/driver_node.py
@@ -2,49 +2,67 @@
 import rclpy
 from rclpy.node import Node
 from std_msgs.msg import Int64
-import struct
-import can
+import serial
+import sys
 import threading
-import time
 
-throttle = 0
-steer = 0
+class DriverNode(Node):
+    def __init__(self):
+        super().__init__("driver")
+        self.throttle = 0
+        self.steer = 0
+        self.init_serial()
+        self.create_subscription(Int64, '/xbox_controller/steer', self.steer_callback, 10)
+        self.create_subscription(Int64, '/xbox_controller/throttle', self.throttle_callback, 10)
 
-def manual_steering_callback(data):
-    global steer
-    steer = data.data
-
-def manual_throttle_callback(data):
-    global throttle
-    throttle = data.data
-
-def main(args=None):
-    bus = can.interface.Bus(bustype='socketcan', channel='can0', bitrate= 250000)
-    
-    rclpy.init(args=args)
-    node = Node("driver")
-
-    subscription_manual_steering = node.create_subscription(Int64,'manual_steering', manual_steering_callback, 10)
-    subscription_manual_throttle = node.create_subscription(Int64,'manual_throttle', manual_throttle_callback, 10)
-
-    thread = threading.Thread(target=rclpy. spin, args=(node, ), daemon=True)
-    thread.start()
- 
-    rate = node.create_rate(20, node.get_clock())
-    while rclpy.ok():
-        print('throttle: %d, steering: %d' % (throttle, steer))
+    def init_serial(self):
         try:
-            can_data = struct.pack('>hhI', throttle, steer, 0)
-            msg = can.Message(arbitration_id=0x1,data=can_data, is_extended_id = False)
-            ret = bus.send(msg)
-        except Exception as error:
-            print("An exception occurred:", error)
-        finally:
-            rate.sleep()
+            self.ser = serial.Serial('/dev/ttyACM0', 115200, timeout=1, write_timeout=1)
+            print(f"Opened {self.ser.name}")
+        except serial.SerialException as e:
+            print(f"Error: {e}")
+            sys.exit(1)
 
-    rclpy.spin(node)
+    def steer_callback(self, msg):
+        self.steer = msg.data
+        print(f"Steer: {self.steer}")
+
+    def throttle_callback(self, msg):
+        self.throttle = msg.data
+        print(f"Throttle: {self.throttle}")
+    
+    def converter(self, throttle, steer):
+        throttle_val = int((self.throttle + 100) * 4095 / 200)
+        steer_val = int((self.steer + 100) * 4095 / 200)
+
+        return throttle_val, throttle_val
+
+    def send_speeds(self):
+        
+        throttle1, throttle2 = self.converter(self.throttle, self.steer)
+
+        try:
+            data = f"{throttle1},{throttle2},{(throttle1 + throttle2) & 0xFFFF}\n"
+            self.ser.write(data.encode())
+            print(f"Sent: {data.strip()}")
+            return self.ser.readline().decode().strip() == "OK"
+        except Exception as e:
+            print(f"Error: {e}")
+            return False
+
+def main():
+    rclpy.init()
+    node = DriverNode()
+    spin_thread = threading.Thread(target=rclpy.spin, args=(node,), daemon=True)
+    spin_thread.start()
+    
+    try:
+        while rclpy.ok():
+            node.send_speeds()
+    except KeyboardInterrupt:
+        node.ser.close()
+    
     rclpy.shutdown()
 
-	
 if __name__ == '__main__':
-	main()
+    main()
--- project-oval/xbox_controller_node.py
+++ project-oval/xbox_controller_node.py
@@ -10,34 +10,34 @@ import time
 stdscr = curses.initscr()
 
 # Throttle max and min
-MAX_MANUAL_THROTTLE_FORWARD = 100
-MAX_MANUAL_THROTTLE_REVERSE = 100
+MAX_THROTTLE_FORWARD = 100
+MAX_THROTTLE_REVERSE = 100
 
-# Steering should be bounded between [-100, +100]
+# steer should be bounded between [-100, +100]
 manual = True
-steering = 0
+steer = 0
 throttle = 0
 switch_time = time.time()
 
 def joy_callback(data):
-	global steering, throttle, manual, switch_time
+	global steer, throttle, manual, switch_time
 
 	# Check for toggle between manual and auto driving (A Button)
 	if(data.buttons[0] == 1 and time.time() - switch_time > 0.1):
 		manual = not manual
 		switch_time = time.time()
-	# Input for throttle and steering are between [-1.0, 1.0]
+	# Input for throttle and steer are between [-1.0, 1.0]
 	throttle_input = data.axes[1]
-	steering_input = data.axes[2]
+	steer_input = data.axes[2]
 
-	# Convert this to [-20, +20] for throttle and [-100, 100] for steering
+	# Convert this to [-20, +20] for throttle and [-100, 100] for steer
 	throttle = int(100 * throttle_input)
-	if throttle > MAX_MANUAL_THROTTLE_FORWARD:
-		throttle = MAX_MANUAL_THROTTLE_FORWARD
-	elif throttle < (-1 * MAX_MANUAL_THROTTLE_REVERSE):
-		throttle = (-1 * MAX_MANUAL_THROTTLE_REVERSE)
+	if throttle > MAX_THROTTLE_FORWARD:
+		throttle = MAX_THROTTLE_FORWARD
+	elif throttle < (-1 * MAX_THROTTLE_REVERSE):
+		throttle = (-1 * MAX_THROTTLE_REVERSE)
 	
-	steering = int(-100 * steering_input)
+	steer = int(-100 * steer_input)
 
 
 def main(args=None):
@@ -47,9 +47,9 @@ def main(args=None):
 
 	# Subscription to joy topic - gets info from controller
 	joy_sub = node.create_subscription(Joy, "joy", joy_callback, 5)
-	# Publishers to manual throttle and steering - publishes bounded number pre-PWM
-	manual_throttle_pub = node.create_publisher(Int64, "manual_throttle", 10)
-	manual_steering_pub = node.create_publisher(Int64, "manual_steer", 10)
+	# Publishers to manual throttle and steer - publishes bounded number pre-PWM
+	throttle_pub = node.create_publisher(Int64, "/xbox_controller/throttle", 10)
+	steer_pub = node.create_publisher(Int64, "/xbox_controller/steer", 10)
 	mode_pub = node.create_publisher(Bool, "/xbox_controller/mode", 10)
 
 	thread = threading.Thread(target=rclpy.spin, args=(node, ), daemon=True)
@@ -66,13 +66,13 @@ def main(args=None):
 				send_data.data = True
 				mode_pub.publish(send_data)
 				# Publishing for actuation
-				manual_throttle_msg = Int64()
-				manual_throttle_msg.data = throttle
-				manual_throttle_pub.publish(manual_throttle_msg)
+				throttle_msg = Int64()
+				throttle_msg.data = throttle
+				throttle_pub.publish(throttle_msg)
 
-				manual_steering_msg = Int64()
-				manual_steering_msg.data = steering
-				manual_steering_pub.publish(manual_steering_msg)
+				steer_msg = Int64()
+				steer_msg.data = steer
+				steer_pub.publish(steer_msg)
 			else:
 				send_data = Bool()
 				send_data.data = False
@@ -81,7 +81,7 @@ def main(args=None):
 			stdscr.refresh()
 			stdscr.addstr(1, 25, 'Xbox Controller       ')
 			stdscr.addstr(2, 25, 'Throttle: %.2f  ' % throttle)
-			stdscr.addstr(3, 25, 'Steering: %.2f  ' % steering)
+			stdscr.addstr(3, 25, 'steer: %.2f  ' % steer)
 			stdscr.addstr(4, 25, 'Manual: %s  ' % str(manual))
 			rate.sleep()
 		except KeyboardInterrupt:
