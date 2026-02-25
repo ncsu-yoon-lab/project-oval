730d1c9269d38f1add13fb017ea7933ec923a9a5
Patrick Story <ptedstoryv2@gmail.com>
2026-02-24 21:38:15 -0500

    improving path point readability

--- project_oval/control/path_following/pure_pursuit/pure_pursuit.py
+++ project_oval/control/path_following/pure_pursuit/pure_pursuit.py
@@ -9,44 +9,68 @@ Current considerations:
 '''
 
 # Point along the path will be a 2D tuple of floats
-Point2D = (float, float)
-class PurePursuit:
+class PathPoint2D:
+    def __init__(self, x, y, cal=0):
+        self.x = x
+        self.y = y
+        
+        # cumulative distance the point is along the path
+        self.cumulative_arc_length = cal
     
+    def set_arc_length(self, cal):
+        self.cumulative_arc_length = cal
+
+    def get_arc_length(self):
+        return self.cumulative_arc_length
+
+    def get_point(self):
+        return (self.x, self.y)
+
+
+class PurePursuit:
     def __init__(self, lookahead_distance):
         self.lookahead_distance = float(lookahead_distance)
 
         self.path = []
-        self.arc_lengths = []
 
         self.x = 0.0
         self.y = 0.0
         self.yaw = 0.0
         self.velocity = 0.0
 
-    def set_path(self, path):
+        self.last_closest_index = 0
+
+    def init_path(self, path):
 
         # init the path.
         self.path = []
         for point in path:
             x = float(point[0])
             y = float(point[1])
-            self.path.append((x, y))
-        
-        # Init the arc lengths along the path
-        self.arc_lengths = []
-        self.arc_lengths.append(0.0)
+            current_point = PathPoint2D(x, y)
 
-        # Compute the cumulative distance along the path
+            self.path.append(current_point)
+
+        # naturally, the first point in the path hase zero cumulative distance
+        self.path[0].set_arc_length(0.0)
+
+        # pre-compute each point's cumulative distance along the path
         for i in range(1, len(self.path)):
-            x0, y0 = self.path[i - 1]
-            x1, y1 = self.path[i]
+            current_point = self.path[i]
+            previous_point = self.path[i-  1]
+
+            x1, y1 = current_point.get_point()
+            x0, y0 = previous_point.get_point()
 
             dx = x1 - x0
             dy = y1 - y0
 
             distance = math.hypot(dx, dy)
-            cumulative = self.arc_lengths[-1] + distance
-            self.arc_lengths.append(cumulative)
+
+            # add distance to last arc length
+            current_total_arc_length = previous_point.get_arc_length() + distance
+            
+            current_point.set_arc_length(current_total_arc_length)
 
         self.last_closest_index = 0
     
