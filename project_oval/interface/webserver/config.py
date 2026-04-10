af8a2e847c85b0e68c1c23d076acfa7454e78238
Patrick Story <ptedstoryv2@gmail.com>
2026-04-10 19:05:30 -0400

    updates to lib

--- project_oval/interface/webserver/config.py
+++ project_oval/interface/webserver/config.py
@@ -2,7 +2,7 @@ from pathlib import Path
 
 # Server
 PORT = 8443
-HOST = "0.0.0.0"
+HOST = "127.0.0.1"
 
 # Data
 DATA_FILE = Path("oval_points.json")
@@ -10,8 +10,7 @@ DATA_FILE = Path("oval_points.json")
 # Car endpoint
 CAR_URL = "https://car.local:8443/path"
 
-# mTLS certs.
-CERT_FILE = "/etc/gateway/webserver.crt"
+# mTLS certs
+CERT_FILE= "/etc/gateway/webserver.crt"
 KEY_FILE = "/etc/gateway/webserver.key"
 CA_FILE = "/etc/gateway/root-ca.crt"
-
